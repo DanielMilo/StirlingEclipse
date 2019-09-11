8208c076c8a398e94b0fca2c5142df2ebad09126
Berndy <al-berndt@gmx.de>
2019-09-12 00:59:21 +0200

    Player Movement including CameraRig that follows player

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    public Transform objectFocus;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+        Turn();
+	}
+
+    void Move()
+    {
+        transform.position = objectFocus.transform.position;
+    }
+
+    void Turn()
+    {
+        Vector3 oldRotation = objectFocus.transform.rotation.eulerAngles;
+        transform.rotation = Quaternion.Euler(0f, oldRotation.y, 0f);
+    }
+    
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float movementSpeed;
+    public float turnSpeed;
+
+    private string verticalAxis;
+    private string horizontalAxis;
+    private float verticalValue;
+    private float horizontalValue;
+
+	// Use this for initialization
+	void Start () {
+        verticalAxis = "Vertical";
+        horizontalAxis = "Horizontal";
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        verticalValue = Input.GetAxis(verticalAxis);
+        horizontalValue = Input.GetAxis(horizontalAxis);
+
+        if(verticalValue < 0)
+        {
+            horizontalValue *= -1;
+        }
+        if(verticalValue != 0f)
+        {
+            Turn();
+        }
+        Move();
+	}
+
+    private void Turn()
+    {
+        float turn = horizontalValue * turnSpeed * Time.deltaTime;
+        Quaternion turnRotation = Quaternion.Euler(0f, turn,  0f);
+        gameObject.transform.rotation *= turnRotation;
+    }
+
+    private void Move()
+    {
+        Vector3 move = transform.forward * verticalValue * movementSpeed * Time.deltaTime;
+
+        gameObject.transform.position += move;
+    }
+}
