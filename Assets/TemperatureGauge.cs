fca81348c74be3d4f20ca21b17ece9f2b4f69950
Berndy <al-berndt@gmx.de>
2019-09-12 18:25:58 +0200

    Tmeperature gauges and test controller

---
+++ Assets/TemperatureGauge.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TemperatureGauge : MonoBehaviour {
+
+    public Slider heatSlider;
+    public Slider coolingSlider;
+    public Slider speedSlider;
+
+    public PlayerMovement player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        heatSlider.minValue = 0;
+        heatSlider.maxValue = 100;
+        coolingSlider.minValue = 0;
+        coolingSlider.maxValue = 100;
+        speedSlider.minValue = player.minSpeed;
+        speedSlider.maxValue = player.maxSpeed;
+
+        heatSlider.value = player.heatValue;
+        coolingSlider.value = player.coolingValue;
+        speedSlider.value = player.CalculateSpeed();
+	}
+}
---
+++ Assets/TestController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestController : MonoBehaviour {
+
+    public float changeSpeed;
+    public PlayerMovement player;
+
+    string heatAxis;
+    string coolingAxis;
+	// Use this for initialization
+	void Start () {
+        heatAxis = "Heat";
+        coolingAxis = "Cooling";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float heatchange = Input.GetAxis(heatAxis);
+        float coolingchange = Input.GetAxis(coolingAxis);
+
+        player.heatValue += heatchange * Time.deltaTime * changeSpeed;
+        player.coolingValue += coolingchange * Time.deltaTime * changeSpeed;
+    }
+}
