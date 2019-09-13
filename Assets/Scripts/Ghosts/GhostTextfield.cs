cbced019505655b8cb89df08cf042d2fd38eae28
Berndy <al-berndt@gmx.de>
2019-09-13 22:45:18 +0200

    Added Nametags to Ghost prefab

---
+++ Assets/Scripts/Ghosts/GhostTextfield.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GhostTextfield : MonoBehaviour
+{
+
+    public Text textfield;
+    GameObject camera;
+    Canvas canvas;
+
+    void Start()
+    {
+        camera = FindObjectOfType<Camera>().gameObject;
+        canvas = GetComponentInChildren<Canvas>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textfield.text = this.name;
+
+        canvas.transform.rotation = camera.transform.rotation;
+    }
+}
