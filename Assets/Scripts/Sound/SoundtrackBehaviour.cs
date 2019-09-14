9e24b77ed3beea8c6eedaf435ce19e1283d1aa78
Daniel Milo <danielmilo12@gmail.com>
2019-09-14 15:15:27 +0300

    soundtrack basic

---
+++ Assets/Scripts/Sound/SoundtrackBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundtrackBehaviour : MonoBehaviour
+{
+    [SerializeField] AudioClip[] clips;
+    AudioClip chosenClip;
+
+    AudioSource source;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+
+        // pick a random clip
+        if(clips.Length > 0)
+        {
+            chosenClip = clips[Random.Range(0, clips.Length)];
+            source.clip = chosenClip;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            if (!source.isPlaying)
+            {
+                source.Play();
+            }
+            source.mute = !source.mute;
+        }
+    }
+}
