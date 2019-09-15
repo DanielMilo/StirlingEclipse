67e3c8fd1797227309a5b1dfc40e1369dcff3300
Berndy <al-berndt@gmx.de>
2019-09-15 19:35:11 +0200

    Added rotor and speedometer

---
+++ Assets/Rotor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotor : MonoBehaviour
+{
+    public GameObject rotator;
+    public float angle;
+    public float minValue;
+    public float maxValue;
+    public float value;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        value = Mathf.Clamp(value, minValue, maxValue);
+
+        Rotate();
+    }
+
+    void Rotate()
+    {
+        float percentage = value / Mathf.Abs(maxValue - minValue);
+        float rotation = (angle * percentage) - (angle / 2);
+
+
+        Vector3 rotatorEuler = rotator.transform.rotation.eulerAngles;
+        rotator.transform.rotation = Quaternion.Euler(rotatorEuler.x, rotatorEuler.y, -rotation);
+    }
+}
---
+++ Assets/Speedometer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Speedometer : MonoBehaviour
+{
+
+    Craft player;
+    Rotor rotor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Craft>();
+        rotor = GetComponent<Rotor>();
+        rotor.minValue = 0;
+        rotor.maxValue = player.speedlimit;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotor.value = player.currentHorizontalSpeed;
+    }
+}
