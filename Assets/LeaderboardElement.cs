94548b12496a499ddebe3f22e6a7476f31f6d92f
Berndy <al-berndt@gmx.de>
2019-09-17 18:30:10 +0200

    Implemented Leaderboard and small tweaks

---
+++ Assets/LeaderboardElement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LeaderboardElement : MonoBehaviour
+{
+    [SerializeField] Text placeText;
+    [SerializeField] Text nameText;
+    [SerializeField] Text timeText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetData(int place, string name, float time)
+    {
+        placeText.text = place + ".";
+        nameText.text = name;
+        timeText.text = ParseTimeToText(time);
+    }
+
+    string ParseTimeToText(float time)
+    {
+        //hours
+        int hours = (int)(time / (60 * 60));
+        time = time % (60 * 60);
+
+        //minutes
+        int minutes = (int)(time / (60));
+        time = time % (60);
+
+        //seconds
+        int seconds = (int)time;
+        int milliseconds = (int)((time % 1) * 100);
+
+        //output
+        string output = AjustNumberLength(hours) + ":" + AjustNumberLength(minutes) + ":" + AjustNumberLength(seconds) + "." + AjustNumberLength(milliseconds);
+        return output;
+    }
+
+    string AjustNumberLength(int x)
+    {
+        if(x < 10)
+        {
+            return "0" + x;
+        }
+        else
+        {
+            return "" + x;
+        }
+    }
+}
---
+++ Assets/LeaderboardManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeaderboardManager : MonoBehaviour
+{
+
+    [SerializeField] GameObject elementPrefab;
+    [SerializeField] int elementCount = 10;
+    [SerializeField] Vector3 boardOffset;
+    [SerializeField] float elementHeight;
+
+    GameObject[] elements;
+    GameController controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        elements = new GameObject[elementCount];
+        for(int index = 0; index < elements.Length; index++)
+        {
+            Vector3 targetPosition = boardOffset;
+            targetPosition.y = boardOffset.y - (elementHeight * index);
+            elements[index] = GameObject.Instantiate(elementPrefab, transform.position + targetPosition, transform.rotation);
+            elements[index].transform.parent = transform;
+        }
+        controller = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int index = 0; index < transform.childCount; index++)
+        {
+            transform.GetChild(index).gameObject.SetActive(controller.isGameOver);
+        }
+    }
+}
