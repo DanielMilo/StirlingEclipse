0d7c70a08b528d7270c7473a4d5c5a426ff919ad
Berndy <al-berndt@gmx.de>
2019-09-21 22:41:55 +0200

    Some level obstacles etc.

---
+++ Assets/MovingPlattfrom.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlattfrom : MonoBehaviour
+{
+    public enum MovementMode
+    {
+        loop, backAndForth, randomized
+    }
+
+    [SerializeField] Transform plattform;
+    [SerializeField] float movementSpeed;
+    [SerializeField] GameObject pointCollection;
+    
+    int currentIndex;
+    int currentIndexModifyer;
+    List<Transform> ralleyPoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetRalleyPoints();
+
+        if(ralleyPoints.Count > 0)
+        {
+            plattform.position = ralleyPoints[0].position;
+            plattform.rotation = ralleyPoints[0].rotation;
+        }
+
+        currentIndex = 1;
+        currentIndexModifyer = 1;
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distanceToTarget = Vector3.Distance(plattform.transform.position, ralleyPoints[currentIndex].position);
+
+        if(distanceToTarget > 0.1f)
+        {
+            Move();
+        }
+        else
+        {
+            ApplyIndexModifyer();
+            Move();
+        }
+    }
+
+    void GetRalleyPoints()
+    {
+        ralleyPoints = new List<Transform>();
+
+        for(int index = 0; index < pointCollection.transform.childCount; index++)
+        {
+            ralleyPoints.Add(pointCollection.transform.GetChild(index));
+            Debug.Log(pointCollection.transform.GetChild(index).gameObject.name);
+        }
+    }
+
+    void ApplyIndexModifyer()
+    {
+        currentIndex += currentIndexModifyer;
+        if(currentIndex >= ralleyPoints.Count)
+        {
+            currentIndex = 0;
+        }
+    }
+
+    void Move()
+    {
+        float distanceToTarget = Vector3.Distance(plattform.transform.position, ralleyPoints[currentIndex].position);
+        float angleToTarget = Quaternion.Angle(plattform.transform.rotation, ralleyPoints[currentIndex].rotation);
+
+        float movementFactor = distanceToTarget / movementSpeed;
+
+        plattform.transform.position = Vector3.MoveTowards(plattform.transform.position, ralleyPoints[currentIndex].position, movementSpeed);
+        plattform.transform.rotation = Quaternion.RotateTowards(plattform.transform.rotation, ralleyPoints[currentIndex].rotation, angleToTarget/movementFactor);
+    }
+
+}
---
+++ Assets/RockSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockSpawner : MonoBehaviour
+{
+
+    [SerializeField] GameObject prefab;
+    [SerializeField] float interval;
+    [SerializeField] float timerOffset;
+    float timer = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = timerOffset;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer >= interval)
+        {
+            timer -= interval;
+            SpawnObject();
+        }
+    }
+
+    void SpawnObject()
+    {
+        GameObject.Instantiate(prefab, transform);
+    }
+}
