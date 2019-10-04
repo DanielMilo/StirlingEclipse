c9222b5185b89d159fc1e0a993ac52c00ed163fa
Berndy <al-berndt@gmx.de>
2019-10-04 04:19:07 +0200

    Added Raindrop Shadow music, musicplayer will now randomly chose a new clip when the current one stopped playing

--- Assets/Scripts/Sound/SoundtrackBehaviour.cs
+++ Assets/Scripts/Sound/SoundtrackBehaviour.cs
@@ -18,24 +18,35 @@ public class SoundtrackBehaviour : MonoBehaviour
         source = GetComponent<AudioSource>();
 
         // pick a random clip
-        if(clips.Length > 0)
-        {
-            chosenClip = clips[Random.Range(0, clips.Length)];
-            source.clip = chosenClip;
-            source.Play();
-        }
+        SelectNewTrack();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(source.time >= source.clip.length)
+        {
+            SelectNewTrack();
+        }
+
         if (Input.GetKeyDown(KeyCode.M))
         {
-            if (!source.isPlaying)
+            /*if (!source.isPlaying)
             {
                 source.Play();
             }
+            */
             source.mute = !source.mute;
+        }
+    }
+
+    void SelectNewTrack()
+    {
+        if(clips.Length > 0)
+        {
+            chosenClip = clips[Random.Range(0, clips.Length)];
+            source.clip = chosenClip;
+            source.Play();
         }
     }
 }
