51c112705ee3bd00396916efe0cb9265d4af8f55
Berndy <al-berndt@gmx.de>
2019-10-09 00:47:00 +0200

    Implemented warning sound and fixed leaderboard GUI

--- Assets/PlayerSoundEffects.cs
+++ Assets/PlayerSoundEffects.cs
@@ -7,17 +7,25 @@ public class PlayerSoundEffects : MonoBehaviour
 {
     [SerializeField] AudioMixerGroup mainMixer;
     [SerializeField] AudioClip pickupClip;
-    [SerializeField] AudioClip criticalClip;
-    [SerializeField] AudioClip chargingClip;
+
+    [Header("Warning")]
+    [SerializeField] AudioClip warningClip;
+    [SerializeField] float minWarningVolume;
+    [SerializeField] float maxWarningVolume;
+
+    [Header("Engine")]
     [SerializeField] AudioClip engineClip;
     [SerializeField] float engineMinPitch;
     [SerializeField] float engineMaxPitch;
     [SerializeField] float enginePitchChangeSpeed;
 
+    //[SerializeField] AudioClip chargingClip;
+
     Craft player;
     GameController controller;
     AudioSource playerAudioSource;
     AudioSource engineSource;
+    AudioSource warningSource;
 
     float defaultPitch;
 
@@ -37,14 +45,23 @@ public class PlayerSoundEffects : MonoBehaviour
             engineSource.outputAudioMixerGroup = mainMixer;
             engineSource.Play();
         }
+
+        warningSource = player.gameObject.AddComponent(typeof(AudioSource)) as AudioSource;
+        if(warningClip != null)
+        {
+            warningSource.clip = warningClip;
+            warningSource.loop = true;
+            warningSource.outputAudioMixerGroup = mainMixer;
+            warningSource.volume = 0;
+            warningSource.Play();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        float targetPitch = engineMinPitch + (engineMaxPitch - engineMinPitch) * player.engine.enginePowerPercentage;
-        float enginePitch = Mathf.Lerp(engineSource.pitch, targetPitch, enginePitchChangeSpeed*Time.deltaTime);
-        engineSource.pitch = enginePitch;
+        EngineSound();
+        WarningSound();
     }
 
     public void OnPickup()
@@ -58,15 +75,26 @@ public class PlayerSoundEffects : MonoBehaviour
         }
     }
 
-    public void OnCritical()
+    void WarningSound()
     {
-        if(criticalClip != null)
+        if(player.heatdeathTimerPercentage > 0f && controller.gameState != GameState.death && controller.gameState != GameState.victory)
         {
-            playerAudioSource.clip = criticalClip;
-            playerAudioSource.loop = true;
-            playerAudioSource.pitch = 1f;
-            playerAudioSource.Play();
+            warningSource.loop = true;
+            warningSource.pitch = 1f;
+            float newVolume = minWarningVolume + (maxWarningVolume - minWarningVolume) * player.heatdeathTimerPercentage;
+            warningSource.volume = newVolume;
         }
+        else
+        {
+            warningSource.volume = 0;
+        }
+    }
+
+    void EngineSound()
+    {
+        float targetPitch = engineMinPitch + (engineMaxPitch - engineMinPitch) * player.engine.enginePowerPercentage;
+        float enginePitch = Mathf.Lerp(engineSource.pitch, targetPitch, enginePitchChangeSpeed * Time.deltaTime);
+        engineSource.pitch = enginePitch;
     }
 
     public void OnCharging(float chargingPercentage)
